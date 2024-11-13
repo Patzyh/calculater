ced21660aed658f5cbcbb986186aa6a936317e41
tjark.lefhalm <tjark.lefhalm@gmail.com>
2024-11-13 12:56:20 +0100

    Main überarbeitet

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from ui import CalculatorUI
 
 def main():
     ctk.set_appearance_mode("System")  # "System", "Dark", "Light"
-    ctk.set_default_color_theme("blue")
 
     root = ctk.CTk()
     app = CalculatorUI(root)
